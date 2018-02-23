108a40f7170eb9022bd80eef7d00c6b73e1aa38e
Jeswin Kumar <jeswinpk@agilehead.com>
2018-02-23 12:28:44 +0530

    WIP: Initial commit

---
+++ src/main.ts
@@ -0,0 +1,69 @@
+import { app, BrowserWindow, Menu, Tray } from "electron";
+import * as path from "path";
+import * as url from "url";
+
+let mainWindow: Electron.BrowserWindow;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "../index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  // Emitted when the window is closed.
+  mainWindow.on("closed", () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", createWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+let tray = null;
+app.on("ready", () => {
+  tray = new Tray(path.join(__dirname, "../assets/icon.png"));
+  const contextMenu = Menu.buildFromTemplate([
+    { type: "separator" },
+    { label: "Item1", type: "radio" },
+    { label: "Item2", type: "radio" },
+    { label: "Item3", type: "radio", checked: true },
+    { label: "Item4", type: "radio" }
+  ]);
+  tray.setToolTip("This is my application.");
+  tray.setContextMenu(contextMenu);
+});
+
+app.on("activate", () => {
+  // On OS X it"s common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app"s specific main process
+// code. You can also put them in separate files and require them here.
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface Todo {
+  id: string,
+  text: string,
+  assignee: string,
+  dueDate: string,
+  dueDateMS: number
+}
+
