5370dcbad46b0de43b48d768808a02929bc55a51
Jeswin Kumar <jeswinpk@agilehead.com>
2018-03-07 00:25:07 +0530

    WIP

--- src/ui/components/networks/RemoteNetwork.tsx
+++ src/ui/components/networks/RemoteNetwork.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { TextField, RaisedButton, FlatButton } from "material-ui";
-import { orange500, blue500 } from "material-ui/styles/colors";
 
 export default function() {
   return (
